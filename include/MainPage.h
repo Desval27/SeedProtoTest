45aadf87a12eb4d118045199a2387f0fd2a6c1f0
Desval27 <paul.f.burdette@gmail.com>
2026-05-08 02:17:49 -0400

    Shift Change

--- include/MainPage.h
+++ include/MainPage.h
@@ -1,85 +1,87 @@
 #pragma once
 
-#include <App.h>
 #include <Pages/BasePage.h>
+#include <SeedProtoTestApp.h>
 #include <daisy.h>
 
-template <typename AppType>
+template<typename AppType>
 class MainPage : public BasePage<true>
 {
-  public:
-    /////////////////////////////////////////////////////////////////////////////
-    /// @brief
-    MainPage() : BasePage(), startPage_(nullptr) {}
+public:
+  /////////////////////////////////////////////////////////////////////////////
+  /// @brief
+  MainPage()
+    : BasePage()
+    , startPage_(nullptr)
+  {
+  }
 
-    /////////////////////////////////////////////////////////////////////////////
-    /// @brief
-    /// @param startPage
-    void Init(daisy::UiPage &startPage) { startPage_ = &startPage; }
+  /////////////////////////////////////////////////////////////////////////////
+  /// @brief
+  /// @param startPage
+  void Init(daisy::UiPage& startPage) { startPage_ = &startPage; }
 
-    /////////////////////////////////////////////////////////////////////////////
-    /// @brief
-    /// @param buttonID
-    /// @param numberOfPresses
-    /// @param isRetriggering
-    /// @return
-    bool OnButton(uint16_t buttonID,
-                  uint8_t  numberOfPresses,
-                  bool     isRetriggering) override
-    {
-        if(buttonID == BUTTON_1)
-        {
-            if(startPage_ == nullptr || numberOfPresses == 0)
-                return false;
-            if(auto *ui = GetParentUI())
-                ui->OpenPage(*startPage_);
-        }
-        return true;
+  /////////////////////////////////////////////////////////////////////////////
+  /// @brief
+  /// @param buttonID
+  /// @param numberOfPresses
+  /// @param isRetriggering
+  /// @return
+  bool OnButton(uint16_t buttonID,
+                uint8_t numberOfPresses,
+                bool isRetriggering) override
+  {
+    if (buttonID == BUTTON_1) {
+      if (startPage_ == nullptr || numberOfPresses == 0)
+        return false;
+      if (auto* ui = GetParentUI())
+        ui->OpenPage(*startPage_);
+    }
+    return true;
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// @brief
+  /// @param potID
+  /// @param newPosition
+  /// @return
+  bool OnPotMoved(uint16_t potID, float newPosition) override
+  {
+    // Pot id corresponds to voice id
+    AppType& theApp = AppType::getInstance();
+    auto* v = theApp.GetVoicePtr(potID);
+    v->config_.volume.SetFrom0to1(newPosition);
+    return true;
+  }
+
+protected:
+  /////////////////////////////////////////////////////////////////////////////
+  /// @brief
+  /// @param display
+  /// @param nowMS
+  void InternalDraw(daisy::OneBitGraphicsDisplay& display,
+                    uint32_t nowMS) override
+  {
+    uint16_t h = display.GetBounds().GetHeight() / 2;
+    daisy::Rectangle rect = display.GetBounds().WithHeight(h);
+    display.SetCursor(26, 0);
+    display.WriteString("DAYSEYE", Font_11x18, true);
+    display.SetCursor(30, 20);
+    display.WriteString("TESTER", Font_11x18, true);
+
+    if ((nowMS % flashPeriodMS_) < flashOnMS_) {
+      display.SetCursor(12, 40);
+      display.WriteString("PRESS OK BUTTON", Font_7x10, true);
     }
 
-    /////////////////////////////////////////////////////////////////////////////
-    /// @brief
-    /// @param potID
-    /// @param newPosition
-    /// @return
-    bool OnPotMoved(uint16_t potID, float newPosition) override
-    {
-        // Pot id corresponds to voice id
-        AppType &theApp = AppType::getInstance();
-        auto *v      = theApp.GetVoicePtr(potID);
-        v->config_.volume.SetFrom0to1(newPosition);
-        return true;
-    }
+    daisy::FixedCapStr<20> text = "TIME:";
+    text.AppendInt(nowMS);
+    display.SetCursor(0, 53);
+    display.WriteString(text.Cstr(), Font_6x8, true);
+  }
 
-  protected:
-    /////////////////////////////////////////////////////////////////////////////
-    /// @brief
-    /// @param display
-    /// @param nowMS
-    void InternalDraw(daisy::OneBitGraphicsDisplay &display,
-                      uint32_t                      nowMS) override
-    {
-        uint16_t         h    = display.GetBounds().GetHeight() / 2;
-        daisy::Rectangle rect = display.GetBounds().WithHeight(h);
-        display.SetCursor(26, 0);
-        display.WriteString("DAYSEYE", Font_11x18, true);
-        display.SetCursor(30, 20);
-        display.WriteString("TESTER", Font_11x18, true);
-
-        if((nowMS % flashPeriodMS_) < flashOnMS_)
-        {
-            display.SetCursor(12, 40);
-            display.WriteString("PRESS OK BUTTON", Font_7x10, true);
-        }
-
-        daisy::FixedCapStr<20> text = "TIME:";
-        text.AppendInt(nowMS);
-        display.SetCursor(0, 53);
-        display.WriteString(text.Cstr(), Font_6x8, true);
-    }
-
-  private:
-    daisy::UiPage *startPage_;
-    const uint32_t flashPeriodMS_ = 1000;
-    const uint32_t flashOnMS_     = flashPeriodMS_ / 2;
+private:
+  daisy::UiPage* startPage_;
+  const uint32_t flashPeriodMS_ = 1000;
+  const uint32_t flashOnMS_ = flashPeriodMS_ / 2;
 };
--- src/Main.cpp
+++ src/Main.cpp
@@ -9,8 +9,8 @@
 #include <ShapeItem.h>
 #include <UIOverlord.h>
 
-#include <App.h>
 #include <MainPage.h>
+#include <SeedProtoTestApp.h>
 #include <StaticPage.h>
 #include <TestPage.h>
 
@@ -24,17 +24,19 @@ using namespace daisy;
 #define VOICE_COUNT 4
 
 using MyApp = App<VOICE_COUNT>;
-using MyOverlord =
-    UIOverlord<SSD130xI2c128x64Driver, ENCODER_COUNT, BUTTON_COUNT, POT_COUNT,
-               ENCODER_1, // MenuEncoder
-               BUTTON_1,  // OK Button
-               BUTTON_2,  // Cancel Button
-               true>;
+using MyOverlord = UIOverlord<SSD130xI2c128x64Driver,
+                              ENCODER_COUNT,
+                              BUTTON_COUNT,
+                              POT_COUNT,
+                              ENCODER_1, // MenuEncoder
+                              BUTTON_1,  // OK Button
+                              BUTTON_2,  // Cancel Button
+                              true>;
 using MyMainPage = MainPage<MyApp>;
 using MyTestPage = TestPage<ENCODER_COUNT, BUTTON_COUNT, POT_COUNT>;
 
 DaisySeed hw;
-MyApp &theApp = MyApp::getInstance();
+MyApp& theApp = MyApp::getInstance();
 
 MyOverlord uiOverlord;
 MyMainPage mainPage;
@@ -47,58 +49,61 @@ FullScreenVerticalMenu<MenuFontSize::FONT_NORMAL> vertMenu1;
 FullScreenVerticalMenu<MenuFontSize::FONT_SMALL> vertMenu2;
 
 const MyOverlord::EncoderConfig encoderConfig[ENCODER_COUNT] = {
-    {seed::D20, seed::D16},
+  { seed::D20, seed::D16 },
 };
 const MyOverlord::ButtonConfig buttonConfig[BUTTON_COUNT] = {
-    {seed::D19}, // Encoder
-    {seed::D17}, {seed::D18}, {seed::D15}, {seed::D21},
+  { seed::D19 }, // Encoder
+  { seed::D17 }, { seed::D18 }, { seed::D15 }, { seed::D21 },
 };
 const MyOverlord::PotConfig potConfig[POT_COUNT] = {
-    {seed::A7},
-    {seed::A8},
-    {seed::A9},
-    {seed::A10},
+  { seed::A7 },
+  { seed::A8 },
+  { seed::A9 },
+  { seed::A10 },
 };
 
 Metro clock;
 HiHat<> hat;
 
-void NOPCallback(void *context) {}
+void
+NOPCallback(void* context)
+{
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // UI & Menu Structure
 ////////////////////////////////////////////////////////////////////////////////
 AbstractMenu::ItemConfig mainMenuItems[] = {
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "CONTROLS",
-     .asOpenUiPageItem{&testPage}},
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "MENU 1",
-     .asOpenUiPageItem{&vertMenu1}},
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "MENU 2",
-     .asOpenUiPageItem{&vertMenu2}},
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "CONTROLS",
+    .asOpenUiPageItem{ &testPage } },
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "MENU 1",
+    .asOpenUiPageItem{ &vertMenu1 } },
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "MENU 2",
+    .asOpenUiPageItem{ &vertMenu2 } },
 
 //
 // This is ugly but works for a test app
 //
 
 #if VOICE_COUNT > 0
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "VOICE 1",
-     .asOpenUiPageItem{&voicePages[0]}},
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "VOICE 1",
+    .asOpenUiPageItem{ &voicePages[0] } },
 #if VOICE_COUNT > 1
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "VOICE 2",
-     .asOpenUiPageItem{&voicePages[1]}},
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "VOICE 2",
+    .asOpenUiPageItem{ &voicePages[1] } },
 #if VOICE_COUNT > 2
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "VOICE 3",
-     .asOpenUiPageItem{&voicePages[2]}},
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "VOICE 3",
+    .asOpenUiPageItem{ &voicePages[2] } },
 #if VOICE_COUNT > 3
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "VOICE 4",
-     .asOpenUiPageItem{&voicePages[3]}},
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "VOICE 4",
+    .asOpenUiPageItem{ &voicePages[3] } },
 #if VOICE_COUNT > 4
 #error "Unsupported VOICE_COUNT"
 #endif
@@ -106,32 +111,32 @@ AbstractMenu::ItemConfig mainMenuItems[] = {
 #endif
 #endif
 #endif
-    {.type = AbstractMenu::ItemType::closeMenuItem, .text = "CLOSE"},
+  { .type = AbstractMenu::ItemType::closeMenuItem, .text = "CLOSE" },
 };
 
 AbstractMenu::ItemConfig vertMenuItems[] = {
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "STATIC",
-     .asOpenUiPageItem{&staticPage}},
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "STATIC",
+    .asOpenUiPageItem{ &staticPage } },
 //
 // This is ugly but works for a test app
 //
 #if VOICE_COUNT > 0
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "VOICE 1",
-     .asOpenUiPageItem{&voicePages[0]}},
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "VOICE 1",
+    .asOpenUiPageItem{ &voicePages[0] } },
 #if VOICE_COUNT > 1
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "VOICE 2",
-     .asOpenUiPageItem{&voicePages[1]}},
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "VOICE 2",
+    .asOpenUiPageItem{ &voicePages[1] } },
 #if VOICE_COUNT > 2
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "VOICE 3",
-     .asOpenUiPageItem{&voicePages[2]}},
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "VOICE 3",
+    .asOpenUiPageItem{ &voicePages[2] } },
 #if VOICE_COUNT > 3
-    {.type = AbstractMenu::ItemType::openUiPageItem,
-     .text = "VOICE 4",
-     .asOpenUiPageItem{&voicePages[3]}},
+  { .type = AbstractMenu::ItemType::openUiPageItem,
+    .text = "VOICE 4",
+    .asOpenUiPageItem{ &voicePages[3] } },
 #if VOICE_COUNT > 4
 #error "Unsupported VOICE_COUNT"
 #endif
@@ -139,38 +144,41 @@ AbstractMenu::ItemConfig vertMenuItems[] = {
 #endif
 #endif
 #endif
-    {.type = AbstractMenu::ItemType::callbackFunctionItem,
-     .text = "ITEM 3",
-     .asCallbackFunctionItem{NOPCallback, (void *)3}},
-    {.type = AbstractMenu::ItemType::callbackFunctionItem,
-     .text = "ITEM 4",
-     .asCallbackFunctionItem{NOPCallback, (void *)4}},
-    {.type = AbstractMenu::ItemType::callbackFunctionItem,
-     .text = "ITEM 5",
-     .asCallbackFunctionItem{NOPCallback, (void *)5}},
-    {.type = AbstractMenu::ItemType::callbackFunctionItem,
-     .text = "ITEM 6",
-     .asCallbackFunctionItem{NOPCallback, (void *)6}},
-    {.type = AbstractMenu::ItemType::callbackFunctionItem,
-     .text = "ITEM 7",
-     .asCallbackFunctionItem{NOPCallback, (void *)7}},
-    {.type = AbstractMenu::ItemType::callbackFunctionItem,
-     .text = "ITEM 8",
-     .asCallbackFunctionItem{NOPCallback, (void *)8}},
-    {.type = AbstractMenu::ItemType::callbackFunctionItem,
-     .text = "ITEM 9",
-     .asCallbackFunctionItem{NOPCallback, (void *)9}},
-    {.type = AbstractMenu::ItemType::callbackFunctionItem,
-     .text = "ITEM 10",
-     .asCallbackFunctionItem{NOPCallback, (void *)10}},
-    {.type = AbstractMenu::ItemType::closeMenuItem, .text = "CLOSE"},
+  { .type = AbstractMenu::ItemType::callbackFunctionItem,
+    .text = "ITEM 3",
+    .asCallbackFunctionItem{ NOPCallback, (void*)3 } },
+  { .type = AbstractMenu::ItemType::callbackFunctionItem,
+    .text = "ITEM 4",
+    .asCallbackFunctionItem{ NOPCallback, (void*)4 } },
+  { .type = AbstractMenu::ItemType::callbackFunctionItem,
+    .text = "ITEM 5",
+    .asCallbackFunctionItem{ NOPCallback, (void*)5 } },
+  { .type = AbstractMenu::ItemType::callbackFunctionItem,
+    .text = "ITEM 6",
+    .asCallbackFunctionItem{ NOPCallback, (void*)6 } },
+  { .type = AbstractMenu::ItemType::callbackFunctionItem,
+    .text = "ITEM 7",
+    .asCallbackFunctionItem{ NOPCallback, (void*)7 } },
+  { .type = AbstractMenu::ItemType::callbackFunctionItem,
+    .text = "ITEM 8",
+    .asCallbackFunctionItem{ NOPCallback, (void*)8 } },
+  { .type = AbstractMenu::ItemType::callbackFunctionItem,
+    .text = "ITEM 9",
+    .asCallbackFunctionItem{ NOPCallback, (void*)9 } },
+  { .type = AbstractMenu::ItemType::callbackFunctionItem,
+    .text = "ITEM 10",
+    .asCallbackFunctionItem{ NOPCallback, (void*)10 } },
+  { .type = AbstractMenu::ItemType::closeMenuItem, .text = "CLOSE" },
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 // Main Audio Loop
 ////////////////////////////////////////////////////////////////////////////////
-void AudioCallback(AudioHandle::InterleavingInputBuffer in,
-                   AudioHandle::InterleavingOutputBuffer out, size_t size) {
+void
+AudioCallback(AudioHandle::InterleavingInputBuffer in,
+              AudioHandle::InterleavingOutputBuffer out,
+              size_t size)
+{
   uiOverlord.ProcessControls();
 
   // Prepare the audio block
@@ -179,8 +187,8 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in,
 
     // float sigL = 0.0f;
     // float sigR = 0.0f;
-    //float sig = hat.Process(trig);
-    auto [sigL, sigR] = theApp.Process();
+    // float sig = hat.Process(trig);
+    auto [sigL, sigR] = theApp.Process(trig);
 
     out[i] = sigL;
     out[i + 1] = sigR;
@@ -188,38 +196,50 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in,
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief 
-/// @param sample_rate 
-void InitComponents(float sample_rate) {
+/// @brief
+/// @param sample_rate
+void
+InitComponents(float sample_rate)
+{
   theApp.Init(sample_rate);
   clock.Init(BPM / 60.0f, sample_rate);
   hat.Init(sample_rate);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief 
-/// @param sample_rate 
-void InitUi(float sample_rate) {
+/// @brief
+/// @param sample_rate
+void
+InitUi(float sample_rate)
+{
   for (std::size_t i = 0; i < theApp.VoiceCount; i++)
     voicePages[i].Init(theApp.GetVoicePtr(i));
 
   mainPage.Init(mainMenu);
-  mainMenu.Init(mainMenuItems, ArrayLen(mainMenuItems),
-                AbstractMenu::Orientation::leftRightSelectUpDownModify, true);
-  vertMenu1.Init(vertMenuItems, ArrayLen(vertMenuItems),
-                 AbstractMenu::Orientation::leftRightSelectUpDownModify, true);
-  vertMenu2.Init(vertMenuItems, ArrayLen(vertMenuItems),
-                 AbstractMenu::Orientation::leftRightSelectUpDownModify, true);
+  mainMenu.Init(mainMenuItems,
+                ArrayLen(mainMenuItems),
+                AbstractMenu::Orientation::leftRightSelectUpDownModify,
+                true);
+  vertMenu1.Init(vertMenuItems,
+                 ArrayLen(vertMenuItems),
+                 AbstractMenu::Orientation::leftRightSelectUpDownModify,
+                 true);
+  vertMenu2.Init(vertMenuItems,
+                 ArrayLen(vertMenuItems),
+                 AbstractMenu::Orientation::leftRightSelectUpDownModify,
+                 true);
 
-  uiOverlord.Init(sample_rate, mainPage, &hw.adc, encoderConfig, buttonConfig,
-                  potConfig);
+  uiOverlord.Init(
+    sample_rate, mainPage, &hw.adc, encoderConfig, buttonConfig, potConfig);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// @brief 
-/// @param  
-/// @return 
-int main(void) {
+/// @brief
+/// @param
+/// @return
+int
+main(void)
+{
   hw.Configure();
   hw.Init();
 
